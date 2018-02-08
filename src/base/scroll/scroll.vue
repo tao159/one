e276542f441e8d4c1035b3129b05b1b0e7efd823
tao159 <yangtao155@163.com>
2018-02-08 17:51:16 +0800

    message

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,58 @@
+<template>
+  <div ref="wrapper">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+
+export default {
+    props:{
+        probeType:{
+            type:Number,
+            default:1
+        },
+        click:{
+            type:Boolean,
+            default:true
+        },
+        data:{
+            type:Array,
+            default:null
+        }
+    },
+    mouted(){
+        setTimeout(()=>{
+            this._initScroll()
+        },20)
+    },
+    methods:{
+        _initScroll(){
+            if(!this.$refs.wrapper){
+                return
+            }
+            this.scroll=new BScroll(this.$refs.wrapper,{
+                probeType:this.probeType,
+                click:this.click
+            })
+        },
+        enable(){
+            this.scroll && this.scroll.enable()
+        },
+        disable(){
+            this.scroll && this.scroll.disable()
+        },
+        refresh(){
+            this.scroll && this.scroll.refresh()
+        }
+    },
+    watch:{
+        data(){
+            setTimeout(()=>{
+                this._initScroll()
+            },20)   
+        }
+    }
+}
+</script>
