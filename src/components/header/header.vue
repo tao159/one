74c16e1d3f52fb1a36858266585de5dab8b9f4f5
tao159 <yangtao155@163.com>
2018-02-22 11:12:42 +0800

    修改配置文件

---
+++ src/components/header/header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="header">
+      <a href="javascript:;" class="left-menu-btn">
+          <slot name='iconW'></slot>
+      </a>
+      {{this.$route.meta.title}}
+  </div>
+</template>
+
+<script type="stext/ecmascript-6">
+  export default {
+    methods:{
+
+    },
+    mounted(){
+      console.log(this.$route.meta.title)
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+   .header
+       height:44px
+       width:100%
+       position:fixed
+       left:0
+       top:0
+       text-align:center
+       line-height:44px
+       font-size:16px
+       font-family:"PingFangSC-Light",sans-serif
+       color:#333
+       z-index:9
+       background:#fff
+       a
+           position:absolute;
+        i
+            font-size:22px
+            padding:10px
+            color:#333
+        .left-menu-btn
+            left:0px
+        .search-wrapper
+            right:0px
+</style>
---
+++ src/components/testbind.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="">
+    <p :class='claName'>测试绑定class</p>
+    <hr>
+    <p :class='{classA:isOk}'>绑定classA并判断，isOk为true时classA生效，false时不生效</p>
+    <hr>
+    <p :class='[claA,claB]'>绑定数组中的class</p>
+    <hr>
+    <p :class='isOk?claC:claD'>绑定class中使用三元表达式判断</p>
+    <hr>
+    <p :style='{color:red,fontSize:font}'>绑定样式</p>
+    <hr>
+    <p :style='styleObj'>用对象方式绑定style</p>
+  </div>
+</template>
+
+<script>
+var styleObj={
+  color:'red',
+  fontSize:'30px'
+}
+export default {
+  data(){
+    return{
+      claName:'className',
+      claA:'classA',
+      claB:'classB',
+      claC:'classC',
+      claD:'classD',
+      isOk:true,
+      styleObj
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  .className{
+    color:red;
+  }
+  .classA{
+    color:gray;
+  }
+  .classB{
+    font-weight:900
+  }
+  .classC{
+    color:blue
+  }
+  .classD{
+    color:lightGreen
+  }
+</style>
